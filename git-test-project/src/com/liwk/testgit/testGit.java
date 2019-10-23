0b2ac193c2ac0924d796e2684cb5ab4ef3fda76b
liuwk <2475950901@qq.com>
2019-10-23 11:22:51 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/liwk/testgit/testGit.java
@@ -0,0 +1,13 @@
+package com.liwk.testgit;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+    }
+}
